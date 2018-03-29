3878da81d62b3ad83c95db968c67fea6d0ea84e5
MSI\Anoeska Clement <a.c.lement6@gmail.com>
2018-03-29 09:16:15 +0100

    kalender opdracht helemaal werkent

--- view/calender/delete.php
+++ view/calender/delete.php
@@ -1,4 +1,4 @@
 <h1>Weet je zeker dat je deze persoon wil verwijderen?</h1>
 <form action="<?=URL?>calender/delete/<?= $calender["id"]?>" method="POST">
-	<input type="submit" name="Yes" value="Yes!">
-	<input type="submit" name="No" value="No!">
+	<input type="submit" name="Yes" value="Yes">
+	<input type="submit" name="No" value="No">
